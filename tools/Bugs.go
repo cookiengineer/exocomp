0c4a3c347933417f122685c6702a5baec7047fdb
Cookie Engineer <cookiengineer@protonmail.com>
2026-03-31 14:47:53 +0200

    :bug: Fixes

--- tools/Bugs.go
+++ tools/Bugs.go
@@ -14,7 +14,7 @@ func NewBugs(agent string, sandbox string) *Bugs {
 		contents: make(map[string]map[string]bool),
 	}
 
-	// readBugs(bugs)
+	readBugs(bugs)
 
 	return bugs
 
@@ -44,27 +44,9 @@ func (tool *Bugs) Call(method string, arguments map[string]interface{}) (string,
 
 		path,   ok1 := arguments["path"].(string)
 		method, ok2 := arguments["method"].(string)
-		note,   ok3 := arguments["note"].(string)
-
-		if ok1 == true && ok2 == true && ok3 == true {
-			return tool.Fix(path, method, note)
-		} else if ok1 == true && ok2 == true && ok3 == false {
-			return "", fmt.Errorf("bugs.%s: %s", method, "Invalid parameter \"note\" is not a string.")
-		} else if ok1 == true && ok2 == false && ok3 == true {
-			return "", fmt.Errorf("bugs.%s: %s", method, "Invalid parameter \"method\" is not a string.")
-		} else if ok1 == false && ok2 == true && ok3 == true {
-			return "", fmt.Errorf("bugs.%s: %s", method, "Invalid parameter \"path\" is not a string.")
-		} else {
-			return "", fmt.Errorf("bugs.%s: %s", method, "Invalid parameters.")
-		}
-
-	} else if method == "FixAll" {
-
-		path,   ok1 := arguments["path"].(string)
-		method, ok2 := arguments["method"].(string)
 
 		if ok1 == true && ok2 == true {
-			return tool.FixAll(path, method)
+			return tool.Fix(path, method)
 		} else if ok1 == true && ok2 == false {
 			return "", fmt.Errorf("bugs.%s: %s", method, "Invalid parameter \"method\" is not a string.")
 		} else if ok1 == false && ok2 == true {
@@ -98,12 +80,8 @@ func (tool *Bugs) Add(path string, method string, note string) (string, error) {
 	return "", fmt.Errorf("bugs.Add: TODO: Not implemented yet")
 }
 
-func (tool *Bugs) Fix(path string, method string, note string) (string, error) {
+func (tool *Bugs) Fix(path string, method string) (string, error) {
 	return "", fmt.Errorf("bugs.Fix: TODO: Not implemented yet")
-}
-
-func (tool *Bugs) FixAll(path string, method string) (string, error) {
-	return "", fmt.Errorf("bugs.FixAll: TODO: Not implemented yet")
 }
 
 func (tool *Bugs) Search(path string, method string) (string, error) {
---
+++ tools/readBugs.go
@@ -0,0 +1,95 @@
+package tools
+
+import "fmt"
+import "os"
+import "strings"
+
+func readBugs(tool *Bugs) error {
+
+	if tool.Sandbox != "" {
+
+		resolved, err0 := resolveSandboxPath(tool.Sandbox, "./BUGS.md")
+
+		if err0 == nil {
+
+			bytes, err1 := os.ReadFile(resolved)
+
+			if err1 == nil {
+
+				for anchor, _ := range tool.contents {
+					delete(tool.contents, anchor)
+				}
+
+				lines := strings.Split(strings.TrimSpace(string(bytes)), "\n")
+
+				for _, line := range lines {
+
+					if strings.TrimSpace(line) == "# Bugs" {
+
+						continue
+
+					} else if strings.TrimSpace(line) == "" {
+
+						continue
+
+					} else if strings.HasPrefix(line, "- [") && strings.Contains(line, "] `") && strings.Contains(line, "`: ") {
+
+						tmp1 := strings.TrimSpace(line[2:strings.Index(line, "] `")])
+						tmp2 := strings.TrimSpace(line[strings.Index(line, "] `"):strings.Index(line, "`: ")])
+						tmp3 := strings.TrimSpace(line[strings.Index(line, "`: "):])
+
+						anchor   := tmp2
+						file     := ""
+						note     := tmp3
+						// method   := ""
+						is_fixed := false
+
+						if strings.Contains(tmp2, "#") {
+							file   = strings.TrimSpace(tmp2[0:strings.Index(tmp2, "#")])
+							// method = strings.TrimSpace(tmp2[strings.Index(tmp2, "#"):])
+						} else {
+							file   = strings.TrimSpace(tmp2)
+							// method = ""
+						}
+
+						if tmp1 == "x" {
+							is_fixed = true
+						}
+
+						if file != "" && note != "" {
+
+							_, err2 := resolveSandboxPath(tool.Sandbox, file)
+
+							if err2 == nil {
+
+								_, ok1 := tool.contents[anchor]
+
+								if ok1 == false {
+									tool.contents[anchor] = make(map[string]bool)
+								}
+
+								tool.contents[anchor][note] = is_fixed
+
+							}
+
+						}
+
+					}
+
+				}
+
+				return nil
+
+			} else {
+				return fmt.Errorf("readBugs: %s", err1.Error())
+			}
+
+		} else {
+			return fmt.Errorf("readBugs: %s", err0.Error())
+		}
+
+	} else {
+		return fmt.Errorf("readBugs: Invalid Tool Sandbox")
+	}
+
+}
