87a15ab6eef7465bb2ff2500e8b8e011f15506d6
Cookie Engineer <cookiengineer@protonmail.com>
2026-04-08 00:13:15 +0200

    :shit: Experimenting with Go Parser/AST

--- tools/Requirements.go
+++ tools/Requirements.go
@@ -1,7 +1,13 @@
 package tools
 
 import "exocomp/utils"
+import "go/ast"
+import "go/parser"
+import "go/printer"
+import "go/token"
+import "bytes"
 import "fmt"
+import "strings"
 
 type Requirements struct {
 	Sandbox string
@@ -28,14 +34,17 @@ func (tool *Requirements) Call(method string, arguments map[string]interface{}) 
 	} else if method == "DefineFunc" {
 
 		// TODO
+		return "", nil
 
 	} else if method == "DefineStruct" {
 
 		// TODO
+		return "", nil
 
 	} else if method == "DefineTest" {
 
 		// TODO
+		return "", nil
 
 	} else if method == "Search" {
 
@@ -59,20 +68,61 @@ func (tool *Requirements) Call(method string, arguments map[string]interface{}) 
 }
 
 func (tool *Requirements) List() (string, error) {
+	return "", nil
 }
 
-func (tool *Requirements) DefineFunc(path string, symbol string, signature string, errors []string, description string) (string, error) {
+func (tool *Requirements) DefineFunc(path string, symbol string, definition string, description string) (string, error) {
+
+	fileset    := token.NewFileSet()
+	file, err0 := parser.ParseFile(fileset, "", strings.Join([]string{
+		"package dummy",
+		definition,
+	}, "\n"), 0)
+
+	if err0 == nil {
+
+		var found *ast.FuncDecl = nil
+
+		for _, decl := range file.Decls {
+
+			declaration, ok0 := decl.(*ast.FuncDecl)
+
+			if ok0 == true {
+
+				if declaration.Type.Params != nil && len(declaration.Type.Params.List) > 0 {
+
+					buffer := bytes.Buffer{}
+					printer.Fprint(&buffer, token.NewFileSet(), decl)
+
+					fmt.Println(buffer.String())
+
+				}
+
+			}
+
+		}
+
+		if found != nil {
+
+			// TODO: Write to a file
+
+		} else {
+		}
+
+	}
+
+	return "", nil
 
 }
 
 func (tool *Requirements) DefineStruct(path string, symbol string, fields []string, description string) (string, error) {
-
+	return "", nil
 }
 
 func (tool *Requirements) DefineTest(path string, symbol string, inputs []string, outputs []string, description string) (string, error) {
-
+	return "", nil
 }
 
 func (tool *Requirements) Search(path string, symbol string) (string, error) {
-
+	return "", nil
 }
