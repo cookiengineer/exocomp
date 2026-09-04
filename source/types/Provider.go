c5bca15b32459b610fa3b8836b6d4c27a3165dcc
Cookie Engineer <cookiengineer@protonmail.com>
2026-09-04 17:49:20 +0200

    :bug: Fix for JSON Marshal/Unmarshal

--- source/types/Provider.go
+++ source/types/Provider.go
@@ -1,5 +1,6 @@
 package types
 
+import "encoding/json"
 import net_url "net/url"
 
 type Provider struct {
@@ -8,3 +9,51 @@ type Provider struct {
 	Token string       `json:"token" yaml:"token"`
 }
 
+func (provider *Provider) MarshalJSON() ([]byte, error) {
+
+	url_str := ""
+
+	if provider.URL != nil {
+		url_str = provider.URL.String()
+	}
+
+	return json.Marshal(struct {
+		URL   string `json:"url"`
+		Alias string `json:"alias"`
+		Token string `json:"token"`
+	}{
+		URL:   url_str,
+		Alias: provider.Alias,
+		Token: provider.Token,
+	})
+
+}
+
+func (provider *Provider) UnmarshalJSON(data []byte) error {
+
+	var tmp struct {
+		URL   string `json:"url"`
+		Alias string `json:"alias"`
+		Token string `json:"token"`
+	}
+
+	err0 := json.Unmarshal(data, &tmp)
+
+	if err0 == nil {
+
+		provider.Alias = tmp.Alias
+		provider.Token = tmp.Token
+
+		tmp_url, err1 := net_url.Parse(tmp.URL)
+
+		if err1 == nil {
+			provider.URL = tmp_url
+		}
+
+		return nil
+
+	} else {
+		return err0
+	}
+
+}
